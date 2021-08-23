de85f661c6cb40714862aabc52516567ca32f393
Ivan Garcia-Sanchez <60237177+Ivan-Garcia-S@users.noreply.github.com>
2021-08-23 14:19:06 -0700

    Update DictionaryTest.java
    Added description of file

--- DictionaryTest.java
+++ DictionaryTest.java
@@ -1,3 +1,4 @@
+//Test client for the Dictionary ADT
 public class DictionaryTest {
 
 	public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class DictionaryTest {
 		 D.insert("2", "red");
 		 D.insert("3", "green");
 		 System.out.println(D.size());
-		// System.out.print(D.head);
+		 // System.out.print(D.head);
 		
 		 System.out.println(D.toString());
 		 D.delete("1");
@@ -16,7 +17,7 @@ public class DictionaryTest {
 		
 		 System.out.println(D.toString());
 		 D.insert("1", "blue");
-	//	 System.out.println(D.size());
+	         // System.out.println(D.size());
 
 		 D.delete("3");
 		 System.out.println(D.size());
@@ -25,19 +26,19 @@ public class DictionaryTest {
 		 D.insert("4", "yellow");
 		 D.insert("5", "purple");
 		 System.out.println(D.toString());
-		// System.out.println(D.tail.toString());
+		 // System.out.println(D.tail.toString());
 		 
-	//	 D.delete("5");
-	//	 System.out.println(D.toString());
+		 // D.delete("5");
+	         // System.out.println(D.toString());
 		 
 		 D.delete("4");
 		 System.out.println(D.toString());
 		 D.delete("5");
 		
 		 D.delete("2");
-				 System.out.println(D.toString());
-				 D.delete("1");
-			 System.out.println(D.toString());
+		 System.out.println(D.toString());
+		 D.delete("1");
+	         System.out.println(D.toString());
 	}
 
 }
